00c263fa4e9de5de8be886d43bb3f72cad697cf0
ivy-nguyen18 <nguyen.ivy1802@knights.ucf.edu>
2021-07-02 02:07:23 -0400

    Layout 1

--- src/main/java/ucf/assignments/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package ucf.assignments;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello Gradle");
-    }
-}
---
+++ src/main/java/ucf/assignments/ListManagerController.java
@@ -0,0 +1,14 @@
+package ucf.assignments;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class ListManagerController {
+
+    private TextField stringDisplay;
+
+    @FXML
+    public void newListButtonClicked(ActionEvent actionEvent) {
+    }
+}
